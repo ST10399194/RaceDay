a6c475c850075388bfddd4240603dc25bdab752f
ST10399194 <ST10399194@RCCONNECT.EDU.ZA>
2026-09-04 00:10:47 +0200

    Create Users Table SQL script

--- Docs/RaceDay_Database.sql
+++ Docs/RaceDay_Database.sql
@@ -7,3 +7,15 @@ GO
 
 USE RaceDay;
 GO
+
+
+
+-- USER TABLE
+CREATE TABLE Users (
+    UserID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Role VARCHAR(20) NOT NULL DEFAULT 'Participant'
+);
